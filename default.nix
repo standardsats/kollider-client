595b11ec483c2f7f26d1e37e9cc0c60c96d6c1a6
Anton Gushcha <ncrashed@protonmail.com>
2021-12-15 16:33:18 +0300

    Initial commit

---
+++ default.nix
@@ -0,0 +1,26 @@
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs-mozilla = import sources.nixpkgs-mozilla;
+  pkgs = import sources.nixpkgs {
+    overlays =
+      [
+        nixpkgs-mozilla
+        (self: super:
+            let chan = self.rustChannelOf { date = "2021-03-01"; channel = "nightly"; };
+            in {
+              rustc = chan.rust;
+              cargo = chan.rust;
+            }
+        )
+      ];
+  };
+  naersk = pkgs.callPackage sources.naersk {};
+  merged-openssl = pkgs.symlinkJoin { name = "merged-openssl"; paths = [ pkgs.openssl.out pkgs.openssl.dev ]; };
+in
+naersk.buildPackage {
+  name = "kollider-api";
+  root = pkgs.lib.sourceFilesBySuffices ./. [".rs" ".toml" ".lock" ".html" ".css" ".png"];
+  buildInputs = with pkgs; [ openssl pkgconfig clang llvm llvmPackages.libclang zlib cacert curl ];
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+  OPENSSL_DIR = "${merged-openssl}";
+}
---
+++ shell.nix
@@ -0,0 +1,19 @@
+with import ./nix/pkgs.nix {};
+let merged-openssl = symlinkJoin { name = "merged-openssl"; paths = [ openssl.out openssl.dev ]; };
+in stdenv.mkDerivation rec {
+  name = "rust-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    rustup
+    clang
+    llvm
+    llvmPackages.libclang
+    openssl
+    cacert
+  ];
+  shellHook = ''
+  export LIBCLANG_PATH="${llvmPackages.libclang}/lib"
+  export OPENSSL_DIR="${merged-openssl}"
+  '';
+}
