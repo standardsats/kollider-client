4c249ed2ec4b59ef08a0cba0e2978ac37c2addf1
Anton Gushcha <ncrashed@protonmail.com>
2022-08-01 11:07:33 +0700

    Add fetch_positions

--- src/kollider/websocket/oneshot.rs
+++ src/kollider/websocket/oneshot.rs
@@ -1,6 +1,7 @@
 use super::client::kollider_websocket;
 use super::data::{
-    make_user_auth, AuthError, BalancesCash, FetchBalancesTag, KolliderMsg, KolliderTaggedMsg,
+    make_user_auth, AuthError, BalancesCash, FetchBalancesTag, FetchPositionsTag, KolliderMsg,
+    KolliderTaggedMsg, Position,
 };
 use crate::kollider::api::Symbol;
 use crate::kollider::client::env::KolliderAuth;
@@ -32,7 +33,7 @@ pub enum Error {
 /// Helper to create oneshot sync requests via websocket. Open socket, request, wait for response, close.
 pub async fn oneshot_ws_request<F, Fut, T>(auth: &KolliderAuth, body: F) -> Result<T, Error>
 where
-    F: FnOnce(UnboundedSender<KolliderMsg>, KolliderMsg) -> Fut + Copy,
+    F: FnOnce(UnboundedSender<KolliderMsg>, KolliderMsg) -> Fut + Clone,
     Fut: Future<Output = Result<Option<T>, Error>>,
 {
     let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
@@ -46,7 +47,7 @@ where
         loop {
             let message = msg_receiver.next().await;
             match message {
-                Some(msg) => match body(stdin_tx.clone(), msg).await {
+                Some(msg) => match body.clone()(stdin_tx.clone(), msg).await {
                     Ok(Some(v)) => return Ok(v),
                     Ok(None) => (),
                     Err(e) => return Err(e),
@@ -63,33 +64,70 @@ where
     }
 }
 
-/// Open websocket and request balances as synchronous request
-pub async fn fetch_balances(auth: &KolliderAuth) -> Result<Balances, Error> {
+/// Open websocket and request positions as synchronous request
+pub async fn oneshot_authed<F, Fut, T>(
+    auth: &KolliderAuth,
+    on_auth: KolliderMsg,
+    body: F,
+) -> Result<T, Error>
+where
+    F: FnOnce(KolliderMsg) -> Fut + Clone,
+    Fut: Future<Output = Option<T>>,
+{
     oneshot_ws_request(auth, |stdin_tx, message| async move {
         match message {
             KolliderMsg::Tagged(KolliderTaggedMsg::Authenticate { message })
                 if message == "success" =>
             {
-                stdin_tx.unbounded_send(KolliderMsg::FetchBalances {
-                    _type: FetchBalancesTag::Tag,
-                })?;
+                stdin_tx.unbounded_send(on_auth)?;
                 Ok(None)
             }
-            KolliderMsg::Tagged(KolliderTaggedMsg::Balances {
-                cash,
-                cross_margin,
-                isolated_margin,
-                order_margin,
-            }) => {
-                return Ok(Some(Balances {
+            _ => Ok(body(message).await),
+        }
+    })
+    .await
+}
+
+/// Open websocket and request balances as synchronous request
+pub async fn fetch_balances(auth: &KolliderAuth) -> Result<Balances, Error> {
+    oneshot_authed(
+        auth,
+        KolliderMsg::FetchBalances {
+            _type: FetchBalancesTag::Tag,
+        },
+        |message| async move {
+            match message {
+                KolliderMsg::Tagged(KolliderTaggedMsg::Balances {
+                    cash,
+                    cross_margin,
+                    isolated_margin,
+                    order_margin,
+                }) => Some(Balances {
                     cash,
                     cross_margin,
                     isolated_margin: isolated_margin.into_iter().map(|(k, v)| (k, v.0)).collect(),
                     order_margin: order_margin.into_iter().map(|(k, v)| (k, v.0)).collect(),
-                }))
+                }),
+                _ => None,
             }
-            _ => Ok(None),
-        }
-    })
+        },
+    )
+    .await
+}
+
+/// Open websocket and request positions as synchronous request
+pub async fn fetch_positions(auth: &KolliderAuth) -> Result<HashMap<Symbol, Position>, Error> {
+    oneshot_authed(
+        auth,
+        KolliderMsg::FetchPositions {
+            _type: FetchPositionsTag::Tag,
+        },
+        |message| async move {
+            match message {
+                KolliderMsg::Tagged(KolliderTaggedMsg::Positions { positions }) => Some(positions),
+                _ => None,
+            }
+        },
+    )
     .await
 }
